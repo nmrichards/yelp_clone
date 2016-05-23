eb6bf65f675cfcf96579af50cdacba1855a45f07
letianw <tmac4ever0824@gmail.com>
2016-05-24 01:36:48 +1000

    closing #1-2 setting up rspec

---
+++ spec/features/restaurants_feature_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature 'restaurants' do
+  context 'no restaurants has been added' do
+    scenario 'should display a prompt to add a restaurant' do
+      expect(page).to have_content 'No restaurants yet'
+      expect(page).to have_link 'Add a restaurant'
+    end
+  end
+end
