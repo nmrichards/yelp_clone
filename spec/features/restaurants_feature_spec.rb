d4c48afc999037e203119e850b2fa8cb4acde24f
letianw <tmac4ever0824@gmail.com>
2016-05-24 17:26:16 +1000

    closing #1-7 displaying restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
+
+  context 'a restaurant has been added' do
+    before(:each) do
+      Restaurant.create(name: 'Papa John')
+    end
+
+    scenario 'display restaurants' do
+      visit 'restaurants'
+      expect(page).to have_content('Papa John')
+      expect(page).not_to have_content('No restaurants yet')
+    end
+  end
 end
