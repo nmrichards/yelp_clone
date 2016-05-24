37e5f822ed3f92a70133e5bb697a83b26f2de17b
Nick <nm.richards@yahoo.com>
2016-05-24 03:20:15 -0700

    View restaurant page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -15,5 +15,9 @@ class RestaurantsController < ApplicationController
   def restaurant_params
     params.require(:restaurant).permit(:name)
   end
-  
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
 end
