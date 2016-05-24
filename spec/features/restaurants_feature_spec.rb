9f2b9b8760baeaa920e7c8348e4c3c0fce6f83f6
letianw <tmac4ever0824@gmail.com>
2016-05-24 22:53:37 +1000

    #closing 1-11 destroying

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -58,4 +58,16 @@ feature 'restaurants' do
       expect(current_path).to eq "/restaurants"
     end
   end
+
+  context 'deleting restaurants' do
+    before { Restaurant.create name: 'KFC', description: 'Deep fried goodness'}
+
+    scenario 'remove a restaurant' do
+      visit '/restaurants'
+      click_link 'Delete KFC'
+      expect(page).not_to have_content 'KFC'
+      expect(page).not_to have_content 'Deep fried goodness'
+      expect(page).to have_content 'Restaurant deleted successfully'
+    end
+  end
 end
