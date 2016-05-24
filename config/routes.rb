f6d109654a40cb1d84fdda19188e0c88a06277ee
letianw <tmac4ever0824@gmail.com>
2016-05-24 16:43:12 +1000

    closing #1-4 routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,13 +8,12 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
+  get 'restaurants' => 'restaurants#index'
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  resource :restaurants
   # Example resource route with options:
   #   resources :products do
   #     member do
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature 'restaurants' do
   context 'no restaurants has been added' do
     scenario 'should display a prompt to add a restaurant' do
+      visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link 'Add a restaurant'
     end
