745e5eff4197ae865347e4a261d913c5286599f8
Amy Nicholson <missamynicholson@gmail.com>
2016-05-25 09:55:22 +0100

    v_2 Ch4

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-resources :restaurants do
-  resources :reviews
-end
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" ,
+                                       :sessions => "users/sessions"}
+  resources :restaurants do
+    resources :reviews
+  end
 
   get 'restaurants' => 'restaurants#index'
 
