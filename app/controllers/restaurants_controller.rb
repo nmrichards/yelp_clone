163186dd08843524e5b6be67f53093773b6a513e
Amy Nicholson <missamynicholson@gmail.com>
2016-05-25 10:13:07 +0100

    Initial commit from day-three

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,65 @@
+class RestaurantsController < ApplicationController
+<<<<<<< HEAD
+=======
+
+>>>>>>> day-two
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+<<<<<<< HEAD
+    Restaurant.create(restaurant_params)
+    redirect_to "/restaurants"
+=======
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render "new"
+    end
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description)
+>>>>>>> day-two
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+<<<<<<< HEAD
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to "/restaurants"
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description)
+  end
+=======
+   @restaurant = Restaurant.find(params[:id])
+   @restaurant.update(restaurant_params)
+
+   redirect_to '/restaurants'
+ end
+
+ def destroy
+   @restaurant = Restaurant.find(params[:id])
+   @restaurant.destroy
+   flash[:notice] = "Restaurant deleted successfully"
+   redirect_to '/restaurants'
+ end
+>>>>>>> day-two
+
+end
