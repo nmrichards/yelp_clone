cfdbdfcece713173035a89127f898c9ad7757847
Jack Riminton <jriminton@gmail.com>
2016-05-25 13:03:37 +0100

    something wasn't right, changed now working

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -243,7 +243,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
-  config.omniauth :facebook, Rails.application.secrets.facebook_app_id, ENV["FACEBOOK_APP_SECRET"]
+  config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"]
                 # callback_url: ""
 
   # ==> Warden configuration
