e370e704c88719ba72b252d031b95210fa8b605c
RichardWatkins1 <richardwatkins1@gmail.com>
2016-05-25 17:54:52 +0100

    final step of the walkthrough 2 tests failing

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,16 @@
 class Restaurant < ActiveRecord::Base
 
   has_many :restaurants
-  has_many :reviews
+  has_many :reviews,
+      -> { extending WithUserAssociationExtension },
+      dependent: :destroy
 
   validates :name, length: { minimum: 3 }, uniqueness: true
+
+  has_many :reviews do
+  def build_with_user(attributes = {}, user)
+    attributes[:user] ||= user
+    build(attributes)
+  end
+end
 end
